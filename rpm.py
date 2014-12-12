f62c7eb3da76998fb6ccf34a9c77597c54abc4e1
Jonathan Sanchez Pando <jsanchezpando@cirb.irisnet.be>
2014-12-12 16:49:38 +0100

    fallback for no host specified

--- rpm.py
+++ rpm.py
@@ -8,22 +8,25 @@ from rpm_fab.main import rpm_build, rpm_install
 
 parser = argparse.ArgumentParser()
 parser.add_argument("action")
-parser.add_argument('-H', '--host', help='increase output verbosity')
+parser.add_argument('-H', '--host', help='increase output verbosity', required=False)
 
 args = parser.parse_args()
+hosts = [args.host or 'localhost']
 
 try:
     action = args.action
     if action == 'build':
         execute(
             rpm_build,
-            hosts=[args.host],
+            hosts=hosts
         )
-    if action == 'install':
+    elif action == 'install':
         execute(
             rpm_install,
-            hosts=[args.host],
+            hosts=hosts
         )
+    else:
+        print "action should be 'build' or 'install'"
 except (EOFError, TypeError) as err:
     print(err)
     sys.exit(1)
