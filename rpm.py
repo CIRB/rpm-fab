0e6360c8679ab1255a918f85c7a34dd30c0b9f0e
Jonathan Sanchez Pando <jsanchezpando@cirb.irisnet.be>
2014-12-15 11:20:54 +0100

    fix env param propagation

--- rpm.py
+++ rpm.py
@@ -3,6 +3,7 @@
 import sys
 import argparse
 from fabric.tasks import execute
+from fabric.state import env
 
 from rpm_fab.main import rpm_build, rpm_install
 
@@ -11,20 +12,14 @@ parser.add_argument('action', choices=('build', 'install'), help='What to do')
 parser.add_argument('-H', '--host', required=False, help='Where to do')
 
 args = parser.parse_args()
-hosts = [args.host] if args.host else None
+env.hosts = [args.host] if args.host else None
 
 try:
     action = args.action
     if action == 'build':
-        execute(
-            rpm_build,
-            hosts=hosts
-        )
+        execute(rpm_build)
     elif action == 'install':
-        execute(
-            rpm_install,
-            hosts=hosts
-        )
+        execute(rpm_install)
 except (EOFError, TypeError) as err:
     print(err)
     sys.exit(1)
